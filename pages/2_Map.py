23b5abb2e64648326f8d6a7b385f7c44da498491
lzrdfrns <167747704+lzrdfrns@users.noreply.github.com>
2026-08-13 20:51:55 +0700

    Update 2_Map.py

--- pages/2_Map.py
+++ pages/2_Map.py
@@ -147,7 +147,7 @@ for _, row in df.iterrows():
             row["Latitude"],
             row["Longitude"]
         ],
-        radius=4,
+        radius=3,
         tooltip=row["Kota"],
         popup=folium.Popup(
             popup_html,
